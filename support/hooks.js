01187c16b3ecf47c49be0b5d77d9b42b63c4d1ac
Tomasz Konieczny <tomasz.konieczny@xsolve.pl>
2017-06-22 15:03:10 +0200

    Proxy support added, HAR logs

--- support/hooks.js
+++ support/hooks.js
@@ -1,9 +1,83 @@
 var world = require('./world.js');
 var driver = require('./world.js').getDriver();
 var config = require('./config.js');
-var fs = require('fs');
-
 var {defineSupportCode} = require('cucumber');
+var fs = require('fs');
+var superagent = require('superagent');
+var sprintf = require('sprintf-js').sprintf;
+var path = require('path');
+
+var proxyPortUrl = 'http://%s:%s/proxy';
+var proxyHarUrl = `http://${ config.proxyHost }:${ config.proxyPort }/proxy/${ config.proxyHttpPort }/har` +
+    `?captureContent=${ config.proxyCaptureContent }&captureHeaders=${ config.proxyCaptureHeaders }`;
+
+var isProxyHttpPortOpen = function() {
+    var requestUrl = sprintf(proxyPortUrl, config.proxyHost, config.proxyPort);
+
+    return superagent
+        .get(requestUrl)
+        .then(function(res) {//TODO: error support (browsermob not running etc.)
+            if(res.status !== 200) {
+                console.log('Proxy response error: ' + res.status);
+                return false;
+            }
+
+            var portsArray = JSON.parse(res.text);
+
+            if( portsArray !== undefined &&
+                portsArray.proxyList !== undefined &&
+                portsArray.proxyList[0] !== undefined &&
+                portsArray.proxyList[0].port !== undefined &&
+                portsArray.proxyList[0].port == config.proxyHttpPort
+            ) {
+                console.log('isProxyHttpPortOpen - true');
+                return true;
+            }
+
+            console.log('isProxyHttpPortOpen - false');
+            return false;
+        });
+};
+
+var openProxyHttpPort = function(proxyHttpPort) {
+    var requestUrl = sprintf(proxyPortUrl, config.proxyHost, config.proxyPort);
+
+    return isProxyHttpPortOpen().then(function(isOpen) {
+        if(!isOpen) {
+            console.log('Opening proxy HTTP port: ' + config.proxyHttpPort);
+
+            return superagent
+                .post(requestUrl)
+                .send('port=' + proxyHttpPort)
+                .then(function(res) {
+                    if(res.status !== 200) {
+                        throw 'Proxy response error: ' + res.status;
+                    }
+
+                    return true;
+                });
+        }
+    });
+};
+
+var startHar = function() {
+    return superagent
+        .put(proxyHarUrl)
+        .then(function(res) {
+            if(res.status !== 200 && res.status !== 204) {
+                throw 'Proxy start HAR error: ' + res.status;
+            }
+
+            return true;
+        });
+};
+
+var saveHar = function(fileName, directory) {
+    var harFilePath = path.join(directory, fileName + ".har");
+
+    return superagent.get(proxyHarUrl)
+        .pipe(fs.createWriteStream(harFilePath));
+};
 
 defineSupportCode(function({After, Before}) {
     var logsDir = 'logs/execution_logs/' + world.getLogsDirName();
@@ -14,6 +88,7 @@ defineSupportCode(function({After, Before}) {
         fs.mkdirSync(logsDir);
     }
 
+    //TODO: screenshot reports
     // if(config.enableScreenshotReports) {
     //     if(!fs.existsSync(screenshotReportsDir)) {
     //         fs.mkdirSync(screenshotReportsDir);
@@ -21,21 +96,20 @@ defineSupportCode(function({After, Before}) {
     // }
 
     Before(function(scenario, callback) {
-        // openProxyHttpPort(world.proxyHttpPort).then(function() {
-        //     startHar();
-        // });
-        console.log('before');
+        openProxyHttpPort(config.proxyHttpPort).then(function() {
+            startHar();
+        });
+
         var featureName = scenario.scenario.feature.name;
         var scenarioName = scenario.scenario.name;
         logFileName = `${ featureName }-${ scenarioName }__${ world.getCurrentDate() }`;
-        console.log('logFileName: ' + logFileName);
+
         callback();
     });
 
     After(function(scenario) {
 
         if(scenario.isFailed()) {
-            console.log('failed');
             world.takeScreenshot(logFileName, logsDir);
 
             driver.manage().logs().get('driver').then(function(logs){
@@ -47,7 +121,9 @@ defineSupportCode(function({After, Before}) {
             });
         }
 
-        return world.cleanBrowserState('', 'logs/execution_logs/');
+        saveHar(logFileName, logsDir);
+
+        return world.cleanBrowserState();
     });
 });
 
