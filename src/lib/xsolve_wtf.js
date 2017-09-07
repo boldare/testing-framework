5eba4d0c27052a23ffe61a0f3112c7292745cef3
Marcin Łesek <daav95@gmail.com>
2017-09-07 15:06:21 +0200

    Update syntax

--- src/lib/xsolve_wtf.js
+++ src/lib/xsolve_wtf.js
@@ -590,7 +590,7 @@ module.exports = {
     getCheckboxValue: getCheckboxValue,
     setCheckboxValue: setCheckboxValue,
     validateCheckboxValue: validateCheckboxValue,
-	getElementText: getElementText,
+    getElementText: getElementText,
     validateElementText: validateElementText,
     getCurrentUrl: getCurrentUrl,
     hover: hover,
